21efa2ba80414444c7c5ae10e5c7a2a98baa2012
Elias Schorr <elias.schorr@fhnw.ch>
2015-09-15 15:04:49 +0200

    crawler init

---
+++ nzCrawler/src/main/java/ch/fhnw/nzcrawler/Application.java
@@ -0,0 +1,16 @@
+package ch.fhnw.nzcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Elias Schorr
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
