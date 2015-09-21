f4df3fe98126495ef440a256d666ce53ea8eb9d3
Elias Schorr <elias.schorr@fhnw.ch>
2015-09-21 09:55:49 +0200

    removed unnecessary imports

--- nzApp/app/src/main/java/fhnw/ch/nzapp/NewsAdapter.java
+++ nzApp/app/src/main/java/fhnw/ch/nzapp/NewsAdapter.java
@@ -1,19 +1,17 @@
 package fhnw.ch.nzapp;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
  * @author Elias Schorr
  * @since 16.09.2015
  */
-public class NewsAdapter extends ArrayAdapter<News> implements NewsListener {
+public class NewsAdapter extends ArrayAdapter<News> {
 
     public NewsAdapter(Context context, News[] resource) {
         super(context, R.layout.row_layout_2, resource);
@@ -31,11 +29,5 @@ public class NewsAdapter extends ArrayAdapter<News> implements NewsListener {
         String imageLink = news.getImageLink();
         new DownloadImageTask(new ImageLoadListener(row.findViewById(R.id.ImageContainer))).execute(imageLink);
         return row;
-    }
-
-    @Override
-    public void setNews(News[] news) {
-        clear();
-        addAll(news);
     }
 }
